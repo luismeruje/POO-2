e42cb8d90e53d326a9ecf7e441778c4edd8d08cc
Diogo Fernandes <treishgame@gmail.com>
2018-02-19 17:57:47 +0000

    faCCCtura

---
+++ JavaFactura/Admin.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Admin here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Admin
+{
+    // instance variables - replace the example below with your own
+    private String password;
+
+    /**
+     * Constructor for objects of class Admin
+     */
+    public Admin()
+    {
+
+    }
+
+    
+}
