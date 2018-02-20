767e8ea439c59463d64934ba8a4e0307f7e20d2d
Jorge <utilizador@brom157.sa.di.uminho.pt>
2018-02-20 13:45:20 +0000

    Projeto no NetBeans

---
+++ NBproject/JavaFactura/src/javafactura/Factura.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Factura here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import java.time.LocalDateTime;
+public class Factura
+{
+    private String id;
+    private int nifEmitente;
+    private String designacao;
+    private LocalDateTime dataDespesa;
+    private int nifCliente;
+    private int descricao;
+    private int atividade;
+    private float valor;
+    /**
+     * Constructor for objects of class Factura
+     */
+    public Factura()
+    {
+       
+    }
+
+    
+}
---
+++ NBproject/JavaFactura/src/javafactura/JavaFactura.java
@@ -0,0 +1,24 @@
+import java.util.*;
+/**
+ * Write a description of class JavaFactura here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class JavaFactura
+{
+    private List<Contribuinte> contribuintes;
+    private List<Factura> facturas;
+    private Admin admin;
+
+    /**
+     * Constructor for objects of class JavaFactura
+     */
+    public JavaFactura()
+    {
+    
+    }
+
+   
+}
